c9d6d9e854eb80008b1afbd7908757a4222e5f9e
SergioCazatti <sergio-c@creer.local>
2022-09-27 16:02:51 -0300

    se agrega javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,34 @@ const divProductos = document.getElementById("divProductos");
 
 productos.forEach((p)=>{
 
-    console.log(p);
+    console.log("producto: " + p.foto);
+    dibujaTarjeta(p);
 
 })
+
+
+function dibujaTarjeta(p){
+
+    divProductos.innerHTML = `
+        <div class="col">
+            <div class="card m-2" >
+                <img src="../${p.foto}" class="card-img-top" alt="Amino">
+                <div class="card-body">
+                    <h5 class="card-title">${p.nombre}</h5>
+                    <p>$${p.precio}</p>
+                    <h5 class="card-title text-center">
+                        <button type="button" class="boton_md" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Agregar
+                        </button>
+                    </h5>
+                </div>
+            </div>
+        </div>                                        
+    `;
+
+
+}
+
 
 /* 
 
